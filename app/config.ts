0fbde9159ec8756c05f4d3c7229a7a6216e8f528
SHAHRON, SHAHFAZLIZ <shahfazliz@gmail.com>
2023-12-18 06:28:19 +0800

    Add app name and description

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,10 @@
 // Path: app/config.ts
 const ALLOWED_APP_FILES_EXTENSIONS = ['js', 'jsx', 'ts', 'tsx', 'json'];
+const APP_DESCRIPTION = 'Monkey Facade';
 const APP_NAME = 'Monkey Facade';
 
 export {
   ALLOWED_APP_FILES_EXTENSIONS,
+  APP_DESCRIPTION,
   APP_NAME
 };
